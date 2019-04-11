013ee1832bd3ac055b6ffd0db47657db38cc214a
Billy <billy@globaltllc.com>
2019-04-12 09:26:54 +1000

    initialized header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from 'react'
+import { Row,Col } from "antd"
+import './index.less'
+
 
 export default class Header extends React.Component {
-    render() {
+    state={}
+    componentWillMount(){
+        this.setState({
+            userName:'Billy'
+        })
+    }
+
+    render(){
+        // const { menuName, menuType } = this.props;
         return (
-            <div>
-                This is header
+            <div className="header">
+                <Row className="header-top">
+
+                    <Col span="6" className="logo">
+                        <img src="/assets/logo-ant.svg" alt=""/>
+                        <span>Bike Management System</span>
+                    </Col>
+                    
+                    <Col>
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">Log out</a>
+                    </Col>
+                </Row>
+                    <Row className="breadcrumb">
+                        <Col span="4" className="breadcrumb-title">
+                            Home
+                        </Col>
+                        <Col span="20" className="weather">
+                            <span className="date">date</span>
+                            <span className="weather-img">
+                            </span>
+                            <span className="weather-detail">
+                                weather
+                            </span>
+                        </Col>
+                    </Row>
+                
             </div>
         );
     }
